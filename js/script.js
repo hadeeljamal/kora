87ed565966dc21dd1d47593e2cc6d86479e5dac3
hadeel <hadeeljamal@users.noreply.github.com>
2022-08-21 14:24:18 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,56 @@
+var swiper = new Swiper(".slide-content", {
+  slidesPerView: 3,
+  spaceBetween: 5,
+  loop: false,
+
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    renderBullet: function (index, className) {
+      return '<span class="' + className + '">' + (index + 1) + '</span>';
+    },
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  breakpoints:{
+      0: {
+          slidesPerView: 1,
+      },
+      520: {
+          slidesPerView: 2,
+      },
+      950: {
+          slidesPerView: 3,
+      },
+  },
+});
+var swiper2 = new Swiper(".slide-content1", {
+  slidesPerView: 3,
+  spaceBetween: 5,
+  loop: false,
+
+  pagination: {
+    el: ".swiper-pagination1",
+    clickable: true,
+    renderBullet: function (index, className) {
+      return '<span class="' + className + '">' + (index + 1) + '</span>';
+    },
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  breakpoints:{
+      0: {
+          slidesPerView: 1,
+      },
+      520: {
+          slidesPerView: 2,
+      },
+      950: {
+          slidesPerView: 3,
+      },
+  },
+});
